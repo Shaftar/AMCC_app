ed2c8d0bb2a7cfb96f07a76570e3c8a27a49893e
Shaftar <shaftar.mohamad@gmail.com>
2020-08-03 16:25:04 +0200

    Create activity_home.xml

--- app/src/main/java/com/example/amcc/HomeActivity.java
+++ app/src/main/java/com/example/amcc/HomeActivity.java
@@ -1,4 +1,15 @@
 package com.example.amcc;
 
-public class HomeActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }

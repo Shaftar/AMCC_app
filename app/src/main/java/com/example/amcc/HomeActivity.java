6acc9a0498855dce11e16e217da11966b049f013
Shaftar <shaftar.mohamad@gmail.com>
2020-08-04 13:34:03 +0200

    Create ListItem.

--- app/src/main/java/com/example/amcc/HomeActivity.java
+++ app/src/main/java/com/example/amcc/HomeActivity.java
@@ -1,15 +1,32 @@
 package com.example.amcc;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
 
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
     }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.app_menu, menu);
+        return true;
+    }
+
 }

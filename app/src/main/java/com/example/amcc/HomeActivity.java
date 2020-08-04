01ae1c2c2614f026f325ad8881ac527efd9cf879
Mohamad Shaftar <47214343+Shaftar@users.noreply.github.com>
2020-08-04 23:07:52 +0200

    Appbar_toolbar (#6)
    * Create ListItem.

--- app/src/main/java/com/example/amcc/HomeActivity.java
+++ app/src/main/java/com/example/amcc/HomeActivity.java
@@ -2,8 +2,12 @@ package com.example.amcc;
 
 import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,5 +15,43 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_home);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+
+        // Trigger a menu item by setting item clickListener
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.iHome:
+                        // Do something
+                        break;
+                    case R.id.iShare:
+                        // Do something
+                        break;
+                    case R.id.iSet:
+                        // Do something
+                        break;
+                }
+                return false;
+            }
+        });
     }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.app_menu, menu);
+        return true;
+    }
+
+
 }

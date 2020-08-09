d235c1c5d60e963bff0c1ebd38856dab5c2c66cf
Shaftar <shaftar.mohamad@gmail.com>
2020-08-09 22:29:22 +0200

    Add setting xml file.

--- app/src/main/java/com/example/amcc/SettingsActivity.java
+++ app/src/main/java/com/example/amcc/SettingsActivity.java
@@ -15,6 +15,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings_container, new SettingsFragment())

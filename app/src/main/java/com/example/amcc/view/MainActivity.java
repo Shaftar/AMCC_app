01dbec78504142c91f2236a92521bf8ad720dcee
Mohamad Shaftar <47214343+Shaftar@users.noreply.github.com>
2020-09-07 12:29:36 +0200

    Adding ads

--- app/src/main/java/com/example/amcc/view/MainActivity.java
+++ app/src/main/java/com/example/amcc/view/MainActivity.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 
 import com.example.amcc.R;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends BaseActivity {
 
@@ -15,11 +13,7 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        AdView adView = new AdView(this);
-        MobileAds.initialize(this, getString(R.string.admob_app_id));
         AdView adView = findViewById(R.id.adView);
-        adView.setAdSize(AdSize.BANNER);
-        adView.setAdUnitId("ca-app-pub-4242963717208905/1233368693");
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
     }

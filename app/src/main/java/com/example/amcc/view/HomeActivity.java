acae3520899f271db5cc5415b2e8c5d7f7a23738
Jaber-aldroubi <47606262+Jaber-aldroubi@users.noreply.github.com>
2020-09-15 20:15:37 +0200

    Single app activity (#42)
    * Make Single app activity

--- app/src/main/java/com/example/amcc/view/HomeActivity.java
+++ app/src/main/java/com/example/amcc/view/HomeActivity.java
@@ -1,22 +1,15 @@
 package com.example.amcc.view;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
-import android.widget.ListView;
 
 import com.example.amcc.R;
-import com.example.amcc.adapter.CustomListAdapter;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 
 public class HomeActivity extends BaseActivity {
 
-    // References to feed our custom List Adapter object
-    String[] nameListArray = {"First Function", "Second Function"};
-    String[] infoListArray = {"info about first function.", "info about second function."};
-    Integer[] imgListArrayID = {R.drawable.car_ins, R.drawable.car_go};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,38 +19,5 @@ public class HomeActivity extends BaseActivity {
         AdView adView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
-    }
-
-    private void setListHomeAdapter() {
-
-        // Create Custom Adapter here
-        CustomListAdapter homeListAdapter = new CustomListAdapter(this, nameListArray, infoListArray, imgListArrayID);
-        // Link listView to our CustomListAdapter
-        //Create list View
-        ListView listView = findViewById(R.id.homelistViewID);
-        listView.setAdapter(homeListAdapter);
-        // Create EventListener
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            switch (position) {
-                case 0:
-                    Intent mainActivity = new Intent(HomeActivity.this, MainActivity.class);
-                    startActivity(mainActivity);
-                    break;
-                case 1:
-                    break;
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Set List Adapter
-        setListHomeAdapter();
-        if (!isOnline()) {
-            connectInternetDialog();
-        } else {
-            connectInternetDialog().dismiss();
-        }
     }
 }
